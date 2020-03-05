e39cd9bcb5d250d9df76ce853bff9c42d0b26501
Trevor Ramey <treramey@gmail.com>
2020-03-05 10:32:51 -0600

    finished javascript-2

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -3,26 +3,33 @@
 
 //////////////////STEP 1////////////////////
 //Create a variable called 'lovesCode' and assign it a value of true or false (as a boolean) depending on if you love code or not. (It's okay if you don't yet!)
-
+ let lovesCode = true;
 // Code Here
 
 //////////////////STEP 2////////////////////
 //Create a variable called 'age' that has a value equal to your age as a number. 
 
 // Code Here
+let age = 26;
 
 //////////////////STEP 3////////////////////
 //Create a variable called 'name' and assign it a value of your name as a string.  
-
+let name = 'Trevor';
 // Code Here
 
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
-
+let phrase = '';
 // Code Here
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
+
+if (lovesCode = true) {
+    phrase = 'I love code'
+}else{
+    phrase = 'I do not love to code'
+}
 
 // Code Here
 
